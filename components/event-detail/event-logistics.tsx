adb9f3e6a13a5f1a9d50561549bd023a595fafdd
Fernando Medina <f.medina@paisanoscreando.com>
2021-05-05 00:29:19 -0300

    adding next/image

--- components/event-detail/event-logistics.tsx
+++ components/event-detail/event-logistics.tsx
@@ -3,34 +3,35 @@ import DateIcon from "../icons/date-icon";
 import LogisticsItem from "./logistics-item";
 import classes from "./event-logistics.module.css";
 import { Event } from "../../types";
+import Image from "next/image";
 
 type EventLogistics = Omit<Event, "description" | "id" | "isFeatured">;
 
 function EventLogistics(props: EventLogistics) {
-  const { date, location, image, title } = props;
+	const { date, location, image, title } = props;
 
-  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
-  const addressText = location.replace(", ", "\n");
+	const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+	const addressText = location.replace(", ", "\n");
 
-  return (
-    <section className={classes.logistics}>
-      <div className={classes.image}>
-        <img src={`/${image}`} alt={title} />
-      </div>
-      <ul className={classes.list}>
-        <LogisticsItem icon={DateIcon}>
-          <time>{humanReadableDate}</time>
-        </LogisticsItem>
-        <LogisticsItem icon={AddressIcon}>
-          <address>{addressText}</address>
-        </LogisticsItem>
-      </ul>
-    </section>
-  );
+	return (
+		<section className={classes.logistics}>
+			<div className={classes.image}>
+				<Image src={`/${image}`} alt={title} width={500} height={500} />
+			</div>
+			<ul className={classes.list}>
+				<LogisticsItem icon={DateIcon}>
+					<time>{humanReadableDate}</time>
+				</LogisticsItem>
+				<LogisticsItem icon={AddressIcon}>
+					<address>{addressText}</address>
+				</LogisticsItem>
+			</ul>
+		</section>
+	);
 }
 
 export default EventLogistics;
--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -10,41 +10,41 @@ import css from "./event-item.module.css";
 type EventItem = Omit<Event, "description" | "isFeatured">;
 
 export default function EventItem(props: EventItem) {
-  const { title, image, date, location, id } = props;
+	const { title, image, date, location, id } = props;
 
-  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
-  const formattedAddress = location.replace(", ", "\n");
-  const exploreLink = `/events/${id}`;
+	const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+	const formattedAddress = location.replace(", ", "\n");
+	const exploreLink = `/events/${id}`;
 
-  return (
-    <li className={css.item}>
-      {/* <Image src={"/" + image} alt={title} width={244} height={224} /> */}
-      <img src={"/" + image} alt={title} />
-      <div className={css.content}>
-        <div className={css.summary}>
-          <h2>{title}</h2>
-          <div className={css.date}>
-            <DateIcon />
-            <time>{humanReadableDate}</time>
-          </div>
-          <div className={css.address}>
-            <AddressIcon />
-            <address>{formattedAddress}</address>
-          </div>
-        </div>
-        <div className={css.actions}>
-          <Button link={exploreLink}>
-            <span> Explore Event</span>
-            <span className={css.icon}>
-              <ArrowRightIcon />
-            </span>
-          </Button>
-        </div>
-      </div>
-    </li>
-  );
+	return (
+		<li className={css.item}>
+			<Image src={"/" + image} alt={title} width={250} height={160} />
+			{/* <img src={"/" + image} alt={title} /> */}
+			<div className={css.content}>
+				<div className={css.summary}>
+					<h2>{title}</h2>
+					<div className={css.date}>
+						<DateIcon />
+						<time>{humanReadableDate}</time>
+					</div>
+					<div className={css.address}>
+						<AddressIcon />
+						<address>{formattedAddress}</address>
+					</div>
+				</div>
+				<div className={css.actions}>
+					<Button link={exploreLink}>
+						<span> Explore Event</span>
+						<span className={css.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
+				</div>
+			</div>
+		</li>
+	);
 }
